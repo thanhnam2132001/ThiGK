fc4f74b5ba0f953b5323f224e14df8f2e2b9548a
Nguyen Thanh Nam <81203413+thanhnam2132001@users.noreply.github.com>
2022-03-21 17:16:53 +0700

    Finish 1,3
    finish

--- 19480161_NguyenThanhNam/app/src/main/java/com/example/a19480161_nguyenthanhnam/Adapter.java
+++ 19480161_NguyenThanhNam/app/src/main/java/com/example/a19480161_nguyenthanhnam/Adapter.java
@@ -1,4 +1,45 @@
 package com.example.a19480161_nguyenthanhnam;
 
-public class Adapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import org.xmlpull.v1.XmlPullParser;
+
+import java.util.ArrayList;
+
+public class Adapter extends BaseAdapter {
+    private Context ctx;
+    private LayoutInflater layout;
+    private ArrayList<Goods> arrayList;
+
+    public Adapter(Context ctx, ArrayList<Goods> arrayList) {
+        this.ctx = ctx;
+        this.arrayList = arrayList;
+        this.layout = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return arrayList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view=LayoutInflater.from(ctx).inflate((XmlPullParser) layout,viewGroup,false);
+
+        return view;
+    }
 }
--- 19480161_NguyenThanhNam/app/src/main/java/com/example/a19480161_nguyenthanhnam/Goods.java
+++ 19480161_NguyenThanhNam/app/src/main/java/com/example/a19480161_nguyenthanhnam/Goods.java
@@ -1,4 +1,67 @@
 package com.example.a19480161_nguyenthanhnam;
 
 public class Goods {
+    private String name;
+    private String ship;
+    private int imageBackGround;
+    private int imageBicycle;
+    private String priceAfter;
+    private String priceBefore;
+
+    public Goods(String name, String ship, int imageBackGround, int imageBicycle, String priceAfter, String priceBefore) {
+        this.name = name;
+        this.ship = ship;
+        this.imageBackGround = imageBackGround;
+        this.imageBicycle = imageBicycle;
+        this.priceAfter = priceAfter;
+        this.priceBefore = priceBefore;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShip() {
+        return ship;
+    }
+
+    public void setShip(String ship) {
+        this.ship = ship;
+    }
+
+    public int getImageBackGround() {
+        return imageBackGround;
+    }
+
+    public void setImageBackGround(int imageBackGround) {
+        this.imageBackGround = imageBackGround;
+    }
+
+    public int getImageBicycle() {
+        return imageBicycle;
+    }
+
+    public void setImageBicycle(int imageBicycle) {
+        this.imageBicycle = imageBicycle;
+    }
+
+    public String getPriceAfter() {
+        return priceAfter;
+    }
+
+    public void setPriceAfter(String priceAfter) {
+        this.priceAfter = priceAfter;
+    }
+
+    public String getPriceBefore() {
+        return priceBefore;
+    }
+
+    public void setPriceBefore(String priceBefore) {
+        this.priceBefore = priceBefore;
+    }
 }
